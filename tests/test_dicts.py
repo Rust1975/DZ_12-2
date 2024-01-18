b41df303e6807d51c0e01426c07491fb094d72dc
Rustem Fayzrakhmanov <rustem-2013@yandex.ru>
2024-01-18 23:06:02 +0500

    Создана функция test_dicts

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -3,6 +3,7 @@ from utils import dicts
 
 def test_dicts():
     assert dicts.get_val({"vcs": "mercurial"}, "vcs") == "mercurial"
+    assert dicts.get_val({"vcs": "mercurial"}, "bazaar") == "git"
     assert dicts.get_val({"vcs": "mercurial"}, "vcs", "mercurial") == "mercurial"
-    assert dicts.get_val({}, "vcs", "mercurial") == {}
+    assert dicts.get_val({}, "vcs", "mercurial") == "mercurial"
     assert dicts.get_val({}, "vcs", "bazaar") == "bazaar"
